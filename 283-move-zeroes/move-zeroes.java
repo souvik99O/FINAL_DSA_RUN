e5a94db4463a0e6aa3d71d34baafa61f785b64c7
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-17 12:52:08 +0530

    Time: 2 ms (83.22%) | Memory: 45.9 MB (80.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,7 +1,48 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int i=0;
+        // int j=i+1;
+        // int zeros=0;
+        // int n= nums.length;
+        
+        // for(int m=0; m<nums.length;m++)
+        // {
+        //     if(nums[m]==0)
+        //     {
+        //         zeros++;
+        //     }
+        // }
+
+    // if(zeros>0){
+    //     while(i<=(n-zeros))
+    //     {
+    //         if(nums[i]==0)
+    //         {
+    //             while(j<nums.length && nums[j]==0 )
+    //             {
+    //                 j++;
+    //             }
+
+    //             if(j<n){
+    //             int c= nums[j];
+    //             nums[j]= nums[i];
+    //             nums[i]=c;
+    //             }
+
+    //             i++;
+    //             j=i;
+    //         }
+    //         else
+    //         {
+    //             i++;
+    //             j++;
+    //         }
+    //     }
+    // }
+
+
         int i=0;
-        int j=0;
+        int j=i+1;
         int zeros=0;
         int n= nums.length;
         
@@ -12,35 +53,29 @@ class Solution {
                 zeros++;
             }
         }
-
-    if(zeros>0){
-        while(i<=(n-zeros))
+    
+    
+    while(i<n-zeros)
+    {
+        if( j<n && nums[i]==0 && nums[j]!=0)
         {
-            if(nums[i]==0)
-            {
-                while(j<nums.length && nums[j]==0 )
-                {
-                    j++;
+           int c= nums[j];
+            nums[j]= nums[i];
+            nums[i]=c;
 
-                }
-
-                if(j<n){
-                int c= nums[j];
-                nums[j]= nums[i];
-                nums[i]=c;
-                }
-
-                i++;
-                j=i;
-            }
-            else
-            {
-                i++;
-                j++;
-            }
+            j++;
+            i++;
+        }
+        else if(j<n && nums[i]==0 && nums[j]==0){
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
         }
     }
-    
+
         
     }
 }
