65ab5e5ad6903be378e83759c8deaf665ac27506
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-17 16:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (76.97%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,26 +1,57 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n= matrix.length;
-        int dupmat[][]=new int [n][n];
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                dupmat[i][j]=matrix[i][j];
-            }
-        }
+        // int dupmat[][]=new int [n][n];
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         dupmat[i][j]=matrix[i][j];
+        //     }
+        // }
 
        
 
-        for(int i=0; i<n; i++)
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         matrix[i][j]=dupmat[n-j-1][i];
+        //     }
+        // }
+
+    //---------------------optimal solution inplace------------------------
+
+    //transpose the matrix
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n;j++)
         {
-            for(int j=0; j<n; j++)
-            {
-                matrix[i][j]=dupmat[n-j-1][i];
-            }
+            int c= matrix[i][j];
+            matrix[i][j]= matrix[j][i];
+            matrix[j][i]=c;
         }
+    }
 
+    //reversing each row of the matrix
 
-        
+    for(int i=0; i<n; i++)
+    {
+        int start=0;
+        int end=n-1;
+        while(start<end)
+        {
+           
+            int c= matrix[i][start];
+            matrix[i][start]= matrix[i][end];
+            matrix[i][end]=c;
+            start++;
+            end--;
+
+        }
+    }
+
+    
     }
 }
