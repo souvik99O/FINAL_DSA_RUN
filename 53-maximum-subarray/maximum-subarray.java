c888870644fc25dbe390b7c2682322a6551f3fbf
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-23 02:09:33 +0530

    Time: 1 ms (99.57%) | Memory: 57.1 MB (56.82%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,14 +1,65 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = nums[0]; // Initialize to first element (handles all-negative case)
-        int sum = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
-            // Choose max between extending the subarray or starting fresh from current element
-            sum = Math.max(nums[i], sum + nums[i]);
-            max = Math.max(max, sum);
-        }
+//O(n2)-------------------------------approach------------------------------------
+// int sum=0;
+// int max=Integer.MIN_VALUE;
+//         for(int i=0; i<nums.length;i++)
+//         {
+//             sum=0;
+//             for(int j=i; j<nums.length; j++)
+//             {
+//                  sum=sum+nums[j];
+//                 if(sum>max)
+//                 max=sum;
+//             }
+//         }
 
-        return max;
+//         return max;
+//O(n2)-------------------------------approach------------------------------------
+
+
+// kedanes algorithm//------------------------------------------------------------
+
+int sum=0;
+int max=Integer.MIN_VALUE;
+if(nums.length==1)
+return nums[0];
+
+
+//actual kedane but it will not handle all negative cases--------------------------------------------
+for(int i=0; i<nums.length;i++)
+{
+    sum+=nums[i];
+    if(sum<0)
+    sum=0;
+
+    if(sum>max)
+    max=sum;
+
+}
+//actual kedane but it will not handle all negative cases--------------------------------------------
+
+if(max==0)
+{
+    max=Integer.MIN_VALUE;
+    for(int i=0; i<nums.length;i++)
+    {
+        if(nums[i]>max)
+        max=nums[i];
+    }
+}
+
+
+
+
+return max;
+
+
+
+
+
+// kedanes algorithm//------------------------------------------------------------
+        
     }
 }
