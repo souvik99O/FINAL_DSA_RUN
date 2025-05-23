cc65b0e6ad43ea7acb66f79f5b132ffdd91bceb3
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-23 20:04:10 +0530

    Time: 8 ms (20.67%) | Memory: 44.4 MB (83.42%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,28 +1,36 @@
 class Solution {
 
 
-     public boolean noRepeats(int l, int r, char[] str) {
-        HashMap<Character, Integer> map = new HashMap<>();
+    //  public boolean noRepeats(int l, int r, char[] str) {
+    //     HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = l; i <= r; i++) {
-            char c = str[i];
-            if (map.containsKey(c)) {
-                return false;  // Duplicate found
-            }
-            map.put(c, 1);
-        }
+    //     for (int i = l; i <= r; i++) {
+    //         char c = str[i];
+    //         if (map.containsKey(c)) {
+    //             return false;  // Duplicate found
+    //         }
+    //         map.put(c, 1);
+    //     }
 
-        return true;  // All characters are unique
-    }
+    //     return true;  // All characters are unique
+    // }
 
 
     public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
         char str[]=s.toCharArray();
         int size=str.length;
         int l=0; int r=0; int maxlen=0;int len=0;
         while(r<size)
         {
-            if(noRepeats(l,r,str))
+            map.put(str[r],map.getOrDefault(str[r],0)+1);
+            while(map.get(str[r])>1){
+                map.put(str[l],map.get(str[l])-1);
+                l++;
+                
+             }
+
+            if(map.get(str[r])==1)
             {
                 len=r-l+1;
                 if(len>maxlen)
@@ -30,10 +38,7 @@ class Solution {
                     maxlen=len;
                 }
             }
-            else{
-                l++;
-                // r--;
-             }
+            
             r++;
         }
         return maxlen;
