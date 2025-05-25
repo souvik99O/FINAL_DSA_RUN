3ca507f2ed1da15108adfc1b411d5c555383a42f
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-26 05:29:27 +0530

    Time: 8 ms (26.02%) | Memory: 46.3 MB (47.04%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,23 +1,96 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
+// TC-> O(n) and SC-> O(n);/////////////////////////////////////////////////////
         
-        HashMap <Integer,Integer> map = new HashMap<>();
+        // HashMap <Integer,Integer> map = new HashMap<>();
 
-        for(int i=0; i<nums.length;i++)
+        // for(int i=0; i<nums.length;i++)
+        // {
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+
+        // System.out.println(map);
+
+        // for(Map.Entry<Integer,Integer> e: map.entrySet())
+        // {
+        //     if(e.getValue()==1)
+        //     return e.getKey();
+        // }
+
+        // return 0;
+
+    // TC-> O(n) and SC-> O(n);/////////////////////////////////////////////////////
+
+
+    // TC-> O(n) and SC-> O(1);////////////------------BIT 
+    
+    
+   // 2 pointers-------------/////////////////////////////////////////
+
+    int l=0; int r=0;
+    Arrays.sort(nums);
+    int count=0;
+    while(r<nums.length)
+    {
+        if(nums[l]==nums[r]){
+        count++;
+        r++;
+        }
+        else
         {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        if(count==1)
+        return nums[l];
+
+         count =0;
+         l=r;
+            
         }
 
-        System.out.println(map);
+    }
 
-        for(Map.Entry<Integer,Integer> e: map.entrySet())
-        {
-            if(e.getValue()==1)
-            return e.getKey();
-        }
+    if(count==1)
+    return nums[l];
 
-        return 0;
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+    // TC-> O(n) and SC-> O(1);////////////------------BIT 
+    
+    
+    
+    
+    //MANUPULATION-------------/////////////////////////////////////////
+
+
+
+
+
+    
+    
+    
+    //MANUPULATION-------------/////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+    // TC-> O(n) and SC-> O(1);////////////------------BIT MANUPULATION-------------/////////////////////////////////////////
+
         
     }
 }
