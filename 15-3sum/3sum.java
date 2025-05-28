908bc318c2ccfddef6c5d65738132244b0b33615
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-29 04:12:11 +0530

    Time: 390 ms (20.58%) | Memory: 52.9 MB (14.98%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,22 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> biglist = new ArrayList<>();
         List<List<Integer>> anslist = new ArrayList<>();
-        List<Integer> zero = new ArrayList<>();
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                count++;
-
-        }
-
-        if (count == nums.length) {
-            for (int i = 0; i < 3; i++) {
-                zero.add(0);
-
-            }
-            anslist.add(zero);
-            return anslist;
-        }
-
+        
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-
+        
+        for (int i = 0; i < nums.length - 2; i++) { // Changed: nums.length-2 to avoid out of bounds
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
             int tempval = 0 - nums[i];
-            biglist = find2sum(nums, tempval, nums[i], i + 1);
+            List<List<Integer>> biglist = find2sum(nums, tempval, nums[i], i + 1);
 
             if (biglist.size() != 0) {
-
                 anslist.addAll(biglist);
             }
         }
         return anslist;
-
     }
 
     public List<List<Integer>> find2sum(int[] nums, int target, int firstval, int start) {
@@ -45,45 +24,25 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = start; i < nums.length; i++) {
+            // Simplified: Remove special target==0 case
+            int findelement = target - nums[i];
 
-            List<Integer> tripple = new ArrayList<>();
-
-            if (target == 0) {
-                if (map.containsKey(-nums[i])) {
-                    tripple.add(firstval);
-                    tripple.add(-nums[i]);
-                    tripple.add(nums[i]);
-                    smalllist.add(tripple);
-
-                    while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-                        i++;
-                    }
-                } else {
-                    map.put(nums[i], i);
+            if (map.containsKey(findelement)) {
+                List<Integer> tripple = new ArrayList<>();
+                tripple.add(firstval);
+                tripple.add(findelement);
+                tripple.add(nums[i]);
+                smalllist.add(tripple);
+                
+                // Skip remaining duplicates after finding a match
+                while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                    i++;
                 }
-
             } else {
-                int findelement = target - nums[i];
-
-                if (map.containsKey(findelement)) {
-                    tripple.add(firstval);
-                    tripple.add(findelement);
-                    tripple.add(nums[i]);
-                    smalllist.add(tripple);
-                    while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-                        i++;
-                    }
-
-                }
-
-                else {
-                    map.put(nums[i], i);
-                }
+                map.put(nums[i], i);
             }
-
         }
 
         return smalllist;
-
     }
 }
