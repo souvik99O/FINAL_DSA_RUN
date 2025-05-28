0661047a081fa9a88fc35b7f56a523934e23cb04
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-29 04:10:04 +0530

    Time: 413 ms (20.21%) | Memory: 52.6 MB (22.63%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,112 +1,89 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> biglist= new ArrayList<>();
-        List<List<Integer>> anslist= new ArrayList<>();
+        List<List<Integer>> biglist = new ArrayList<>();
+        List<List<Integer>> anslist = new ArrayList<>();
         List<Integer> zero = new ArrayList<>();
-        int count=0;
+        int count = 0;
 
-        for(int i=0; i<nums.length;i++)
-        {
-            if(nums[i]==0)
-            count++;
-        
-            
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                count++;
+
         }
 
-        if(count==nums.length){
-            for(int i=0; i<3;i++)
-        {
-          zero.add(0);
-        
-            
-        }
-        anslist.add(zero);
-        return anslist;
+        if (count == nums.length) {
+            for (int i = 0; i < 3; i++) {
+                zero.add(0);
+
+            }
+            anslist.add(zero);
+            return anslist;
         }
 
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
 
-             if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
-            int tempval=0-nums[i];
-            biglist=find2sum(nums,tempval,nums[i],i+1);
 
-            if(biglist.size()!=0)
-            {
-                
+            int tempval = 0 - nums[i];
+            biglist = find2sum(nums, tempval, nums[i], i + 1);
+
+            if (biglist.size() != 0) {
+
                 anslist.addAll(biglist);
             }
         }
         return anslist;
-       
+
     }
 
+    public List<List<Integer>> find2sum(int[] nums, int target, int firstval, int start) {
+        List<List<Integer>> smalllist = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for (int i = start; i < nums.length; i++) {
 
-   public List<List<Integer>> find2sum(int[] nums,int target,int firstval,int start) 
-   { 
-        List<List<Integer>> smalllist= new ArrayList<>();
-        HashMap<Integer,Integer> map= new HashMap<>();
-
-        for(int i=start; i<nums.length;i++)
-        {
-           
-            
             List<Integer> tripple = new ArrayList<>();
-            
-            if(target==0)
-            {
-                     if(map.containsKey(-nums[i]))
-                        {
-                            tripple.add(firstval);
-                            tripple.add(-nums[i]);
-                            tripple.add(nums[i]);
-                            smalllist.add(tripple);
 
-                              while (i + 1 < nums.length && nums[i] == nums[i + 1])
-                                {
-                                    i++;
-                                }
-                        }
-                        else
-                        {
-                            map.put(nums[i],i);
-                        }
+            if (target == 0) {
+                if (map.containsKey(-nums[i])) {
+                    tripple.add(firstval);
+                    tripple.add(-nums[i]);
+                    tripple.add(nums[i]);
+                    smalllist.add(tripple);
 
+                    while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                        i++;
+                    }
+                } else {
+                    map.put(nums[i], i);
+                }
 
-            }
-            else
-            {
-                int findelement=target-nums[i];
+            } else {
+                int findelement = target - nums[i];
 
-                if(map.containsKey(findelement))
-                {
+                if (map.containsKey(findelement)) {
                     tripple.add(firstval);
                     tripple.add(findelement);
                     tripple.add(nums[i]);
                     smalllist.add(tripple);
-                    while (i + 1 < nums.length && nums[i] == nums[i + 1])
-                                {
-                                    i++;
-                                }
+                    while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                        i++;
+                    }
 
                 }
-                
-                else
-                {
-                    map.put(nums[i],i);
+
+                else {
+                    map.put(nums[i], i);
                 }
             }
 
         }
 
+        return smalllist;
 
-return smalllist;
-
-   }
+    }
 }
