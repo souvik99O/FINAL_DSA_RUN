6763ec32032a1775a477080545b711f2fc2c1f85
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-29 18:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (77.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int i=m;
-        int j=0;
+        // int i=m;
+        // int j=0;
 
         // while(i<m+n && j<n)
         // {
@@ -34,16 +34,39 @@ class Solution {
              
         // }
 
-        while(i<m+n)
-        {
-            nums1[i]=nums2[j];
-            i++;
-            j++;
-        }
-        Arrays.sort(nums1);
+        // while(i<m+n)
+        // {
+        //     nums1[i]=nums2[j];
+        //     i++;
+        //     j++;
+        // }
+        // Arrays.sort(nums1);
 
+              // Initialize pointers
+        int i = m - 1;        // Pointer to last valid element in nums1
+        int j = n - 1;        // Pointer to last element in nums2
+        int k = m + n - 1;    // Pointer to last position in nums1 array
         
-      
+        // Merge arrays from the end while both arrays have elements
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                // nums1 element is larger, place it at position k
+                nums1[k] = nums1[i];
+                i = i - 1;    // Move nums1 pointer backward
+                k = k - 1;    // Move result pointer backward
+            } else {
+                // nums2 element is larger or equal, place it at position k
+                nums1[k] = nums2[j];
+                j = j - 1;    // Move nums2 pointer backward
+                k = k - 1;    // Move result pointer backward
+            }
+        }
+          while (j >= 0) {
+            nums1[k] = nums2[j];
+            j = j - 1;
+            k = k - 1;
+        }
         
+
     }
 }
