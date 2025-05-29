3093db29f5ddbff6bb54882dd9b88bf9aa120c3c
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-29 18:08:23 +0530

    Time: 1 ms (27.55%) | Memory: 42.5 MB (27.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,43 +1,49 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int i=0; int j=0;
-        int k=m;
-      
-        if (n>0) { 
-            while(i<m+n && j<n) {
-                
-                if(i >= k && nums2[j] != 0) {  // If we're in the empty space
-                    nums1[i] = nums2[j];
-                    i++;
-                    j++;
-                }
-                else if(i < k && nums2[j] < nums1[i]) {  // Insert nums2[j] before nums1[i]
-                    // Shift elements to make space
-                    for(int l = k; l > i; l--) {
-                        nums1[l] = nums1[l-1];
-                    }
-                    nums1[i] = nums2[j];
-                    k++;  // One more element in the valid portion
-                    i++;
-                    j++;
-                }
-                else if(i < k && nums1[i] <= nums2[j]) {  // nums1[i] is smaller or equal
-                    i++;
-                }
-                else {  // Handle equal case when i >= k
-                    nums1[i] = nums2[j];
-                    i++;
-                    j++;
-                }
-            }
-            
-            // Copy any remaining elements from nums2
-            while(j < n) {
-                nums1[k] = nums2[j];
-                k++;
-                j++;
-            }
+        int i=m;
+        int j=0;
+
+        // while(i<m+n && j<n)
+        // {
+          
+        //     if(i<m+n && j<n &&  nums1[i]!=0 && nums1[i]<nums2[j])
+        //     {
+        //         i++;
+        //     }
+
+
+        //      if(i<m+n && j<n && nums1[i]==0 && nums2[j]!=0)
+        //         {
+        //             nums1[i]=nums2[j];
+        //             i++;
+        //             j++;
+        //          }
+
+
+        //     if(i<m+n && j<n && (nums1[i]>nums2[j] || nums1[i]==nums2[j]))
+        //     {
+        //         for(int l=k; l>i;l--)
+        //         {
+        //             nums1[l]=nums1[l-1];
+        //         }
+        //         nums1[i]=nums2[j];
+        //         i++;j++;k++;
+        //     }
+
+             
+        // }
+
+        while(i<m+n)
+        {
+            nums1[i]=nums2[j];
+            i++;
+            j++;
         }
+        Arrays.sort(nums1);
+
+        
+      
+        
     }
 }
