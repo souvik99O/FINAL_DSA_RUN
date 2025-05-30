b6e5c5470655584da7db48c66e8b33090c01909d
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-05-30 17:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (86.75%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -17,29 +17,16 @@ class Solution {
             len++;
             temp=temp.next;
         }
-
-        
-
-        int even=0;
-        int odd=0;
         int count=0;
         int middle=(len/2)+1;
-        System.out.println(len);
-        System.out.println(middle);
-       
-
         temp=head;
         while(temp!=null)
         {
             count++;
             if(count==middle)
-            {
-                break;
-            }
+            break;
             temp=temp.next;
         }
-
-        return temp;
-        
+        return temp; 
     }
 }
