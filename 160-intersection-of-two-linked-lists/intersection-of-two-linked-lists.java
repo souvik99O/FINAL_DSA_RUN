8be651ca2e15bf2b055afb744a878f999231f9ae
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-02 03:31:53 +0530

    Time: 1 ms (99.88%) | Memory: 49 MB (23.77%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,117 +9,119 @@
  *     }
  * }
  */
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        // Alternative Stack-based Approach----------------------------------------------------------------
+        /*
+        Stack<Integer> stA = new Stack<Integer>();
+        Stack<Integer> stB = new Stack<Integer>();
+        
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        
+        // Push all values from both lists into respective stacks
+        while(tempA != null || tempB != null) {
+            if(tempA != null) {
+                stA.push(tempA.val);
+                tempA = tempA.next;
+            }
+            if(tempB != null) {
+                stB.push(tempB.val);
+                tempB = tempB.next;
+            }
+        }
+        
+        // Reset pointers to traverse lists again
+        tempA = headA;
+        tempB = tempB;
+        
+        // Find intersection by comparing stack tops with current nodes
+        while(tempA != null || tempB != null) {
+            if(stA.peek() == stB.peek() && (tempA.val == stA.peek() || tempB.val == stA.peek()))
+                break;
+            if(stA.peek() == stB.peek()) {
+                stA.pop();
+                stB.pop();
+            }
+            if(tempA != null)
+                tempA = tempA.next;
+            if(tempB != null)
+                tempB = tempB.next;
+        }
+        
+        if(tempA != null && tempA.val == stA.peek())
+            return tempA;
+        if(tempB != null && tempB.val == stA.peek())
+            return tempB;
+        
+        return null;
+        */
+        
+        // Current Implementation (Two-Pointer Approach)
+        // Implementation goes here...
+//     }
+// }
+
+// Alternative Stack-based Approach----------------------------------------------------------------
+
+
+
+
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
-    //     Stack<Integer> stA= new Stack<Integer>();
-    //     Stack<Integer> stB= new Stack<Integer>();
-
-    //     ListNode tempA= headA;
-    //     ListNode tempB= headB;
-
-    //     while(tempA!=null || tempB!=null)
-    //     {
-    //         if(tempA!=null)
-    //         {
-    //             stA.push(tempA.val);
-    //             tempA=tempA.next;
-    //         }
-    //         if(tempB!=null)
-    //         {
-    //             stB.push(tempB.val);
-    //             tempB=tempB.next;
-    //         }
-
-    //     }  
-
-
-    //     tempA= headA;
-    //     tempB= headB;
-
-    //     while(tempA!=null || tempB!=null)
-    //     {
-    //         if(stA.peek()==stB.peek() && (tempA.val==stA.peek()||tempB.val==stA.peek()))
-    //         break;
-    //         if(stA.peek()==stB.peek())
-    //         {
-    //             stA.pop();
-    //             stB.pop();
-    //         }
-    //         if(tempA!=null)
-    //         tempA=tempA.next;
-    //         if(tempB!=null)
-    //         tempB=tempB.next;
-    //     }
-
-    //     if(tempA!=null && tempA.val==stA.peek())
-    //     return tempA;
-    //     if(tempB!=null && tempB.val==stA.peek()) 
-    //     return tempB;
-
+        if(headA == headB) 
+            return headA;
         
-
-    // return null;
-
-    ListNode f=null;
-    ListNode s=null;
-    ListNode tempA= headA;
-    ListNode tempB= headB;
-    int lenA=0;int lenB=0;
-
-    if(headA==headB)
-    return headA;
-
-    while(tempA!=null || tempB!=null){
-            {
-                if(tempA!=null)
-                {
-                    lenA++;
-                    tempA=tempA.next;
-                }
-                if(tempB!=null)
-                {
-                    lenB++;
-                    tempB=tempB.next;
-                }
-
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int lenA = 0, lenB = 0;
+        
+        // Calculate lengths of both lists
+        while(tempA != null || tempB != null) {
+            if(tempA != null) {
+                lenA++;
+                tempA = tempA.next;
             }
-    }
-
-    System.out.println(lenA); 
-    System.out.println(lenB);
-
-    if(lenA>lenB)
-    {
-        f=headA;
-        s=headB;
-    }
-    else
-    {
-        f=headB;
-        s=headA;
-    }
-
-    int diff= Math.abs(lenA-lenB);
-    System.out.println(diff);
-
-    while(diff!=0)
-    {
-        f=f.next;
-        diff--;
-    }
-
-    while(f!=null && s!=null )
-    {
-        if(f==s)
-        break;
-
-        f=f.next;
-        s=s.next;
-    }
-
-    
-    return f;
-
+            if(tempB != null) {
+                lenB++;
+                tempB = tempB.next;
+            }
+        }
+        
+        // Set pointers: f for longer list, s for shorter list
+        ListNode f = (lenA > lenB) ? headA : headB;
+        ListNode s = (lenA > lenB) ? headB : headA;
+        
+        // Advance the longer list by the difference
+        int diff = Math.abs(lenA - lenB);
+        while(diff != 0) {
+            f = f.next;
+            diff--;
+        }
+        
+        // Find intersection point
+        while(f != null && s != null) {
+            if(f == s)
+                break;
+            f = f.next;
+            s = s.next;
+        }
+        
+        return f;
     }
 }
+    
