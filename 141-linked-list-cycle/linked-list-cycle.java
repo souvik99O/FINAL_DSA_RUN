5916d67bea78759581490b0db64605513ce739ec
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-11 00:50:02 +0530

    Time: 1 ms (18.45%) | Memory: 45 MB (11.74%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,23 +11,45 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null )
-        {
-            return false;
-        }
 
-        HashSet<ListNode> set= new HashSet<>();
-        ListNode temp=head;
+//brute force using hash sets---------------------------------------      
+        // if(head==null || head.next==null )
+        // {
+        //     return false;
+        // }
+
+        // HashSet<ListNode> set= new HashSet<>();
+        // ListNode temp=head;
         
-        while(temp!=null)
-        {
-            if(set.contains(temp)){
-            return true;
-            }
+        // while(temp!=null)
+        // {
+        //     if(set.contains(temp)){
+        //     return true;
+        //     }
             
-            set.add(temp);
-            temp=temp.next;
-        }
+        //     set.add(temp);
+        //     temp=temp.next;
+        // }
+        // return false;
+//brute force using hash sets--------------------------------------- 
+
+//Little more optimized---------------------------------------------
+
+    if(head==null || head.next==null )
+    {
         return false;
+    }
+
+    ListNode temp=head;
+
+    while(temp!=null)
+    {
+        if(temp.val==-50265)
+        return true;
+        temp.val=-50265;
+        temp=temp.next;
+    }
+    return false;
+
     }
 }
