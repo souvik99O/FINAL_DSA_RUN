39f1ad35074b4ff94145dfbda8f3fd15b830b56a
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-11 16:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (22.32%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,55 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode temp = head;
-        ListNode prev = null;
-        ListNode front = null;
-        ListNode counter = null;
-        int count = k;
-        ListNode link = head;  // This will point to the tail of first group
 
-        // First group reversal
-        while(count != 0) {
-            front = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = front;
+        ListNode temp=head;
+        ListNode prev=null;
+        ListNode front=null;
+        ListNode counter=null;
+        int count=k;
+        ListNode link=head;
+
+        while(count!=0)
+        {
+            front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+
             count--;
         }
-        head = prev;
+        head=prev;
 
-        while(temp != null) {
-            counter = temp;
-            count = 0;
-            prev = null;
+
+
+
+        while(temp!=null){
+            counter=temp;
+            count=0;
+            prev=null;
             
-            // Check if we have k nodes
-            while(counter != null && count != k) {
-                counter = counter.next;
+             
+            while(counter!=null && count!=k)
+            {
+                counter=counter.next;
                 count++;
             }
-            
-            if(counter == null && count != k) {
-                link.next = temp;  // Connect remaining nodes
+            if(counter==null && count!=k)
+            {
+                link.next = temp; 
                 return head;
             }
-            
-            // CHANGE: Store the current node (will become tail after reversal)
-            ListNode groupTail = temp;
-            
-            // Reverse the k nodes
-            while(temp != counter) {
-                front = temp.next;
-                temp.next = prev;
-                prev = temp;
-                temp = front;
+
+            ListNode groupstail=temp;
+
+            while(temp!=counter)
+            {
+                front=temp.next;
+                temp.next=prev;
+                prev=temp;
+                temp=front;
             }
-            
-            // CHANGE: Connect previous group's tail to current group's head
-            link.next = prev;
-            // CHANGE: Update link to current group's tail
-            link = groupTail;
+
+            link.next=prev;
+            link=groupstail;
         }
 
-        return head;
+    return head;
+        
     }
 }
