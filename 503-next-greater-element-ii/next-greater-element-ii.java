1a9d0f143b4de7e994cdb9d1aecae64b96ab8c27
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-13 23:01:40 +0530

    Time: 18 ms (28.38%) | Memory: 46.4 MB (48.89%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,29 +4,35 @@ class Solution {
         int arr[]= new int[nums.length];
         Stack<Integer> st= new Stack();
 
+        // Traverse twice to handle circular array - go from right to left
         for(int i=2*nums.length-1; i>=0;i--)
         {
+            // Map back to original array using modulo
             int index=i%(nums.length);
 
-                    if(!st.empty())
-                    {
-                            while(!st.empty()&&nums[index]>=st.peek())
-                            {
-                                st.pop();
-                            }
-                    }
+            // Pop smaller elements from stack - they can't be next greater
+            if(!st.empty())
+            {
+                while(!st.empty()&&nums[index]>=st.peek())
+                {
+                    st.pop();
+                }
+            }
 
-                    if(st.empty() && i<nums.length)
-                    {
-                     arr[i]=-1;   
-                    }
-                    else
-                    {
-                        if(i<nums.length)
-                        {
-                            arr[i]=st.peek();
-                        }
-                    }
+            // Store result only for first pass through array
+            if(st.empty() && i<nums.length)
+            {
+             arr[i]=-1;   
+            }
+            else
+            {
+                if(i<nums.length)
+                {
+                    arr[i]=st.peek();
+                }
+            }
+            
+            // Push current element to maintain decreasing stack
             st.push(nums[index]);
         }
         
@@ -35,3 +41,10 @@ class Solution {
         
     }
 }
+
+// i=5, index=2, nums[2]=1: stack=[] → arr[2] not set, stack=[1]
+// i=4, index=1, nums[1]=2: stack=[1] → pop 1, arr[1] not set, stack=[2]  
+// i=3, index=0, nums[0]=1: stack=[2] → arr[0] not set, stack=[2,1]
+// i=2, index=2, nums[2]=1: stack=[2,1] → arr[2]=2, stack=[2,1,1]
+// i=1, index=1, nums[1]=2: stack=[2,1,1] → pop 1,1, arr[1]=-1, stack=[2,2]
+// i=0, index=0, nums[0]=1: stack=[2,2] → arr[0]=2, stack=[2,2,1]
