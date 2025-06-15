0bc6020f9e34fd7195e2e98592071f37cd0ae1df
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-15 20:36:21 +0530

    Time: 5 ms (38.51%) | Memory: 44.7 MB (92.17%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,54 +1,33 @@
 class MinStack {
-    Stack<Integer> st= new Stack();
-    LinkedList <Integer> list;
-    int top;
-    int min;
+    Stack<Integer> dataStack;
+    Stack<Integer> minStack;
+
     public MinStack() {
-        min=Integer.MAX_VALUE;
-        top=-1;
-       list=new LinkedList(); 
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
     }
-    
-   public void push(int val) {
-    list.add(val);
-    if (st.isEmpty()) {
-        st.push(val);
-        min = val;
-    } else {
-        if (val < min) min = val;
-        st.push(Math.min(val, st.peek()));
-    }
-}
 
-    
-  public void pop() {
-        if (!st.isEmpty()) {
-            st.pop();
+    public void push(int val) {
+        dataStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
         }
-        if (!list.isEmpty()) {
-            list.removeLast();
-        }
-        min = st.isEmpty() ? Integer.MAX_VALUE : st.peek(); // always update min
     }
-    
+
+    public void pop() {
+        if (!dataStack.isEmpty()) {
+            dataStack.pop();
+            minStack.pop();
+        }
+    }
+
     public int top() {
-        int l=list.peekLast();
-        return l;
-        
+        return dataStack.peek();
     }
-    
+
     public int getMin() {
-       
-        return min;
-        
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
