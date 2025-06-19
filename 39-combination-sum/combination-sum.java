901a15cf9bb7ed439ac2ddd913bab6c907db4b3b
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-19 13:13:41 +0530

    Time: 2 ms (83.69%) | Memory: 44.5 MB (81.07%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -3,32 +3,34 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> mainList = new ArrayList<>();
-        
+        int sum=0;
 
-        f(0,target,list,candidates.length,candidates,mainList);
+        f(0,sum,list,candidates.length,candidates,mainList,target);
 
         return mainList;
 
     }
 
-    public static void f(int i, int t, List<Integer>list, int n, int [] candidates,List<List<Integer>> mainList)
+    public static void f(int i, int sum, List<Integer>list, int n, int [] candidates,List<List<Integer>> mainList,int t)
     {
         //base
-        if(t==0)
+        if(i>=n)
         {
+            if(sum==t)
             mainList.add(new ArrayList(list));
-            return;
+
+          return;  
         }
-        if(i>=n || t<0)
+        if(sum>t)
         return;
         //base
 
         //pick and keep picking till target is not negative
         list.add(candidates[i]);
-        f(i,t-candidates[i],list,n,candidates,mainList);
+        f(i,sum+candidates[i],list,n,candidates,mainList,t);
         //donot pick that index move to the next
         list.remove(list.size()-1);
-        f(i+1,t,list,n,candidates,mainList);
+        f(i+1,sum,list,n,candidates,mainList,t);
         
     }
 }
