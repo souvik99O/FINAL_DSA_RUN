ad1faefa353765ddff6b8f620ad86c0ab6d1179a
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-25 10:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (23.45%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,38 +13,58 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+
+//         if(root==null)
+//         {
+//             return true;
+//         }
+//         int flag=0;
+
+
+        
+
+//         boolean left=isBalanced(root.left);
+
+//         int check=Math.abs(maxDepth(root.left)-maxDepth(root.right));
+//         if(check<=1)
+//         flag=1;
+//         else 
+//         flag=0;
+        
+//         boolean right=isBalanced(root.right);
+
+//         if(left == true && right ==true && flag==1)
+//         return true;
+//         else
+//         return false;
+        
+        
+
+
+
+
+        
+//     }
+//      public int maxDepth(TreeNode root) {
+
+//         if(root==null)
+//         return 0;
+
+//         int left=maxDepth(root.left);
+//         int right=maxDepth(root.right);
+//         int MaxDepth=1+ Math.max(left,right);
+//         return MaxDepth;
+//     }
+// }
 class Solution {
     public boolean isBalanced(TreeNode root) {
 
-        if(root==null)
-        {
-            return true;
-        }
-        int flag=0;
-
-
-        
-
-        boolean left=isBalanced(root.left);
-
-        int check=Math.abs(maxDepth(root.left)-maxDepth(root.right));
-        if(check<=1)
-        flag=1;
-        else 
-        flag=0;
-        
-        boolean right=isBalanced(root.right);
-
-        if(left == true && right ==true && flag==1)
+        if(maxDepth(root)>-1)
         return true;
         else
         return false;
-        
-        
-
-
-
-
         
     }
      public int maxDepth(TreeNode root) {
@@ -54,6 +74,13 @@ class Solution {
 
         int left=maxDepth(root.left);
         int right=maxDepth(root.right);
+
+        if(left==-1 || right == -1)
+        return -1;
+
+        if(Math.abs(left-right)>1)
+        return -1;
+
         int MaxDepth=1+ Math.max(left,right);
         return MaxDepth;
     }
