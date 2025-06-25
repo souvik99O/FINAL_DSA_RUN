b344336dc60a97322bfcda978d7da7e8ef90a3a3
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-06-25 06:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (27.37%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,16 +13,44 @@
  *     }
  * }
  */
+
+ //RECURSIVE ----------------------------------------------------------
+
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+
+//         if(root==null)
+//         return list;
+
+//         list.add(root.val);
+//         List<Integer> left=preorderTraversal(root.left);
+//         List<Integer> right=preorderTraversal(root.right);
+//         return list;
+//     }
+// }
+
+//IERATIVE USING STACK-------------------------------------------
 class Solution {
     List<Integer> list = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-
+        List<Integer> list = new ArrayList<>();
         if(root==null)
         return list;
+        Stack<TreeNode> st= new Stack<>();
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode printElement=st.pop();
+            list.add(printElement.val);
 
-        list.add(root.val);
-        List<Integer> left=preorderTraversal(root.left);
-        List<Integer> right=preorderTraversal(root.right);
+            if(printElement.right!=null)
+            st.push(printElement.right);
+
+            if(printElement.left!=null)
+            st.push(printElement.left);
+        }
         return list;
+
     }
 }
