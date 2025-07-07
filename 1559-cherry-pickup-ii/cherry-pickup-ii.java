d669c62a7f107c34410c498da3be749d90e9c114
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-08 00:51:02 +0530

    Time: 43 ms (11.12%) | Memory: 45.8 MB (82.97%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,59 +1,53 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
 
-        int rows=grid.length;
-        int cols=grid[0].length;
-        int dp[][][]= new int[rows][cols][cols];
-        for(int i=0; i<rows;i++)
-        {
-            for(int j=0; j<cols;j++)
-            {
-                for(int k=0; k<cols;k++)
-                {
-                    dp[i][j][k]=-1;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int dp[][][] = new int[rows][cols][cols];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == j)
+                    dp[rows - 1][i][j] = grid[rows - 1][j];
+
+                else
+                    dp[rows - 1][i][j] = grid[rows - 1][i] + grid[rows - 1][j];
+
+            }
+        }
+
+        
+
+        for (int i = rows - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < cols; j1++) {
+                for (int j2 = 0; j2 < cols; j2++) {
+
+                    int max=Integer.MIN_VALUE;
+
+                    for (int dj1 = -1; dj1 < 2; dj1++) {
+                        for (int dj2 = -1; dj2 < 2; dj2++) {
+
+                            if (j1 + dj1 >=0 && j1 + dj1 <= cols - 1 && j2 + dj2 > 0 && j2 + dj2 <= cols - 1) {
+                                int cherry = 0;
+                                if (j1 == j2)
+                                    cherry = grid[i][j1];
+                                else
+                                    cherry = grid[i][j1] + grid[i][j2];
+
+                                cherry = cherry + dp[i + 1][j1 + dj1][j2 + dj2];
+
+                                max = Math.max(max, cherry);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = max;
+
                 }
             }
         }
-        int ans=solve(0,0,cols-1,grid,rows,cols,dp);
-        return ans;
+
+        return dp[0][0][cols - 1];
+
     }
-    public int solve(int i,int j1,int j2,int [][]grid,int rows,int cols,int [][][]dp)
-    {
-        if(i>rows-1||j1>cols-1||j1<0 ||j2>cols-1||j2<0 )
-        return Integer.MIN_VALUE;
 
-        if(i==rows-1)
-        {
-            if(j1==j2)
-            {
-                return grid[i][j1];
-            }
-            else
-            {
-                return grid[i][j1]+grid[i][j2];
-            }
-        }
-
-        if(dp[i][j1][j2]!=-1)
-        return dp[i][j1][j2];
-
-        int max = Integer.MIN_VALUE;
-
-        for(int dj1=-1; dj1<2;dj1++)
-        {
-            for(int dj2=-1;dj2<2;dj2++)
-            {
-            int cherry=0;
-             if(j1==j2)
-             cherry=grid[i][j1];
-             else
-             cherry=grid[i][j1]+grid[i][j2];
-
-             cherry=cherry + solve(i+1,j1+dj1,j2+dj2,grid,rows,cols,dp);
-             max=Math.max(max,cherry);
-            }
-        }
-
-        return dp[i][j1][j2]=max;
-    }
 }
