58f5500e6b02e09bc3e73e27547cbd1c76c6bd89
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-08 13:34:44 +0530

    Time: 205 ms (5.10%) | Memory: 71.9 MB (8.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,59 +1,46 @@
 class Solution {
     public boolean canPartition(int[] nums) {
 
-        int totalSum=0;
-    
-        for(int i=0; i<nums.length;i++)
-        {
-            totalSum+=nums[i];
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
         }
 
-        int dp[][]=new int[nums.length+1][totalSum+1];
+        int dp[][] = new int[nums.length + 1][totalSum + 1];
 
-        for(int i=0; i<dp.length;i++)
-        {
-            for(int j=0; j<dp[0].length;j++)
-            {
-                dp[i][j]=-1;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
             }
         }
 
-        boolean ans= solve(0,0,totalSum,nums,dp);
+        boolean ans = solve(0, 0, totalSum, nums, dp);
         return ans;
     }
 
-
-    public boolean solve(int ind, int p1sum,int totalSum,int nums[],int[][] dp)
-    {
-        int n=nums.length;
-        if(ind>=n)
-        {
-            if(p1sum==(totalSum-p1sum))
-            return true;
+    public boolean solve(int ind, int p1sum, int totalSum, int nums[], int[][] dp) {
+        int n = nums.length;
+        if (ind >= n) {
+            if (p1sum == (totalSum - p1sum))
+                return true;
             return false;
         }
 
-        if(dp[ind][p1sum]!=-1)
-        {
-            if(dp[ind][p1sum]==1)
-            return true;
+        if (dp[ind][p1sum] != -1) {
+            if (dp[ind][p1sum] == 1)
+                return true;
             return false;
         }
 
+        boolean pick = solve(ind + 1, p1sum + nums[ind], totalSum, nums, dp);
+        boolean notPick = solve(ind + 1, p1sum, totalSum, nums, dp);
 
-    
-        boolean pick=solve(ind+1,p1sum+nums[ind],totalSum,nums,dp);
-        boolean notPick=solve(ind+1,p1sum,totalSum,nums,dp);
-
-
-        if(pick==true|| notPick==true)
-        {
-            dp[ind][p1sum]=1;
+        if (pick == true || notPick == true) {
+            dp[ind][p1sum] = 1;
             return true;
-        }
-        else
-        {
-            dp[ind][p1sum]=0;
+        } else {
+            dp[ind][p1sum] = 0;
         }
         return false;
 
