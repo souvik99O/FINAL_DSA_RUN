89420fc706d487d65fb9a18d2c17e50543e23d1b
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-11 20:07:05 +0530

    Time: 459 ms (70.94%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,9 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 SELECT w1.id
 FROM Weather w1, Weather w2
-where w1.recordDate-1=w2.recordDate
+where dateDiff(w1.recordDate,w2.recordDate)=1
 and w1.temperature > w2.temperature;
+
+-- SELECT w1.Id
+-- FROM Weather w1, Weather w2
+-- WHERE dateDiff(w1.recordDate,w2.recordDate) = 1 AND w1.Temperature > w2.Temperature;
