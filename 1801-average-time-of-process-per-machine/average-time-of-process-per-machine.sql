038cf0012abc0d45cd9305fe6b66657e6a534f04
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-12 14:12:12 +0530

    Time: 251 ms (38.84%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-
-
 with ref as
 (
 select  a1.machine_id ,a1.process_id ,(a1.timestamp  - a2.timestamp ) as time 
@@ -8,8 +6,3 @@ from  Activity a1, Activity a2 where a1.machine_id=a2.machine_id and a1.process_
 order by machine_id
 )
 select machine_id, round(avg(time),3) as processing_time from ref group by machine_id;
-
-
--- select  a1.machine_id ,a1.process_id ,(a1.timestamp  - a2.timestamp ) as time 
--- from  Activity a1, Activity a2 where a1.machine_id=a2.machine_id and a1.process_id=a2.process_id and a1.activity_type = 'end' AND a2.activity_type = 'start'
--- order by machine_id
