1e9312519f83698d413d63f92c2c324378bb522c
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-20 01:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (67.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,16 @@
 class Solution {
 
-//---------------------------------o(n) brute  force------------------------------
+    //---------------------------------o(n) brute  force------------------------------
 
     // public int[] searchRange(int[] arr, int target) {
-        
+
     //     int low=0;
     //     int high=arr.length-1;
     //     int ans[]= new int[2];
     //     ans[0]=-1;ans[1]=-1; 
     //     if(arr.length==0)
     //     {return ans;}
-       
+
     //    for(int i=0; i<arr.length;i++)
     //    {
     //     if(arr[i]==target)
@@ -28,65 +28,40 @@ class Solution {
     // }
 
     public int[] searchRange(int[] arr, int target) {
-     
-     int low=0;
-     int high=arr.length-1;
-     int ans[]=new int[2];
-     ans[0]=-1;
-     ans[1]=-1;
 
-     while(low<=high)
-     {
-        int mid=(high+low)/2;
-        if(arr[mid]>target)
-        {
-            high=mid-1;
-        }
-        else if(arr[mid]<target)
-        {
-            low=mid+1;
-        }
-        else
-        {
-            if(mid>0 && arr[mid-1]==arr[mid])
-            high=mid-1;
-            else{
-            ans[0]=mid;
-            break;
+        int low = 0;
+        int high = arr.length - 1;
+        int ans[] = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (arr[mid] > target) {
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                ans[0] = mid;
+                high = mid - 1;
             }
         }
 
-     }
-
-
-     low=0;
-     high=arr.length-1;
-     while(low<=high)
-     {
-        int mid=(high+low)/2;
-        if(arr[mid]>target)
-        {
-            high=mid-1;
-        }
-        else if(arr[mid]<target)
-        {
-            low=mid+1;
-        }
-        else
-        {
-            if(mid < arr.length - 1 && arr[mid+1]==arr[mid])
-            low=mid+1;
-            else
-            {
-            ans[1]=mid;
-            break;
+        low = 0;
+        high = arr.length - 1;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (arr[mid] > target) {
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                ans[1] = mid;
+                low = mid + 1;
             }
         }
 
-     }
+        return ans;
 
-     return ans;
-
-        
     }
 }
