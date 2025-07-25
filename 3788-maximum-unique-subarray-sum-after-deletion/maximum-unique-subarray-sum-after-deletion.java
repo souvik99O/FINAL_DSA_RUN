c7c185b1f5a09ebc523985fb5a2e6d608afd1eb2
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-25 18:53:56 +0530

    Time: 2 ms (74.06%) | Memory: 42.5 MB (64.69%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        HashSet<Integer> set= new HashSet<>();
+        int negCount=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<nums.length;i++)
+        {
+            if(nums[i]<0)
+            negCount++;
+            max=Math.max(max,nums[i]);
+        }
+        if(negCount==nums.length)
+        return max;
+
+        if(nums.length==1)
+        return nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>0)
+            {
+                set.add(nums[i]);
+            }
+        }
+        int ans=0;
+        for(int val:set)
+        {
+            ans+=val;
+        }
+
+        return ans;
+        
+    }
+}
