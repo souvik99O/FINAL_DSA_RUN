9e1fe6b919f987c0c4ce23a44973a8cee1a6e6bf
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-25 15:59:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (66.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,26 +1,59 @@
 class Solution {
     public void setZeroes(int[][] mat) {
-        int col[] = new int[mat[0].length];
-        int row[] = new int[mat.length];
+       
+        boolean FrowZ=true;
+        boolean FcolZ=true;
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 0) {
-                    if (row[i] != -1)
-                        row[i] = -1;
-                    if (col[j] != -1)
-                        col[j] = -1;
+        for(int i=0; i<mat.length;i++)
+        {
+            if(mat[i][0]==0)
+            FcolZ=false;
+        }
+
+        for(int j=0; j<mat[0].length;j++)
+        {
+            if(mat[0][j]==0)
+            FrowZ=false;
+        }
+
+        for(int i=1; i<mat.length;i++)
+        {
+            for(int j=1; j<mat[0].length;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    mat[0][j]=0;
+                    mat[i][0]=0;
                 }
             }
         }
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (row[i] == -1 || col[j] == -1) {
-                    mat[i][j] = 0;
+        for(int i=1; i<mat.length;i++)
+        {
+            for(int j=1; j<mat[0].length;j++)
+            {
+                if(mat[i][0]==0 || mat[0][j]==0)
+                {
+                    mat[i][j]=0;
                 }
             }
         }
 
+        if(FrowZ==false)
+        {
+            for(int j=0; j<mat[0].length;j++)
+            {
+                mat[0][j]=0;
+            
+            }
+        }
+        if(FcolZ==false)
+        {
+            for(int i=0; i<mat.length;i++)
+            {
+                mat[i][0]=0;
+              
+            }  
+        }
     }
 }
