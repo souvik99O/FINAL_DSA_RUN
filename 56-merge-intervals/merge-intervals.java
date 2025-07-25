330168fdf1979fb625be37d525056d9ded6bd140
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-25 18:02:36 +0530

    Time: 9 ms (45.01%) | Memory: 46.9 MB (25.54%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,61 @@
+class Pair{
+    int start;
+    int end;
+
+    public Pair(int start, int end)
+    {
+        this.start=start;
+        this.end=end;
+    }
+
+}
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-      ArrayList<int[]> list = new ArrayList<>(Arrays.asList(intervals));
-        
-       for(int i=0; i<list.size();i++)
-       {
-        if(i!=0 && list.get(i)[0]<=list.get(i-1)[1])
-        {
-            if( list.get(i)[1]<=list.get(i-1)[1])
-            {
-                list.remove(i);;
-            }
-            else{
-            list.get(i-1)[1]=list.get(i)[1];
-            list.remove(i);
-            }
-            i--;
-        }
-       }
+ List<Pair> list= new ArrayList<>(); 
+  List<Pair> Anslist= new ArrayList<>(); 
 
-       intervals=list.toArray(new int [0][0]);
-       return intervals;
+        for(int i=0; i<intervals.length;i++)
+        {
+            Pair p= new Pair(intervals[i][0],intervals[i][1]);
+            list.add(p);
+        }
+
+        Collections.sort(list,(a,b)->a.start-b.start);
+
+        int minStart=list.get(0).start;
+        int maxEnd=list.get(0).end;
+
+        for(int i=0; i<list.size();i++)
+        {
+            if(list.get(i).start<=maxEnd)
+            {
+                maxEnd=Math.max(maxEnd,list.get(i).end);
+            }
+            else
+            {
+                Anslist.add(new Pair(minStart,maxEnd));
+                minStart=list.get(i).start;
+                maxEnd=list.get(i).end;
+            }
+        }
+
+        Pair p= new Pair(minStart,maxEnd);
+
+        if(!Anslist.contains(p))
+        Anslist.add(p);
+
+    int result[][]= new int[Anslist.size()][2];
+
+    for(int i=0; i<Anslist.size();i++)
+    {
+        result[i][0]=Anslist.get(i).start;
+        result[i][1]=Anslist.get(i).end;
+    }
+
+    return result;
+
+        
+        
     }
 }
