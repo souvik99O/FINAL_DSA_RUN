c67f31be9404855a1aa68cd97499eb50735e9509
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-27 00:47:33 +0530

    Time: 3 ms (82.80%) | Memory: 42.9 MB (22.71%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,68 +1,86 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class pair {
+    int horizontalDistance;
     TreeNode node;
-    int HD;
-    int level;
 
-    pair(TreeNode node, int HD, int level) {
+    public pair(int horizotalDistance, TreeNode node) {
+        this.horizontalDistance = horizotalDistance;
         this.node = node;
-        this.HD = HD;
-        this.level = level;
     }
 }
 
-class nodeInfo {
-    int level;
-    int value;
+class nodeinfo {
+    int val;
+    int lvl;
 
-    nodeInfo(int level, int value) {
-        this.level = level;
-        this.value = value;
+    public nodeinfo(int val, int lvl) {
+        this.val = val;
+        this.lvl = lvl;
     }
 }
 
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ansList = new ArrayList<>();
-        HashMap<Integer, List<nodeInfo>> mainMap = new HashMap<>();
         Queue<pair> q = new LinkedList<>();
-        q.offer(new pair(root, 0, 0));
 
+        TreeMap<Integer, List<nodeinfo>> map = new TreeMap<>();
+
+        q.offer(new pair(0, root));
+        int lvl=0;
         while (!q.isEmpty()) {
-            pair p = q.poll();
-            int HD = p.HD;
-            int level = p.level;
-
-            mainMap.putIfAbsent(HD, new ArrayList<>());
-            mainMap.get(HD).add(new nodeInfo(level, p.node.val));
-
-            if (p.node.left != null)
-                q.offer(new pair(p.node.left, HD - 1, level + 1));
-            if (p.node.right != null)
-                q.offer(new pair(p.node.right, HD + 1, level + 1));
+            lvl++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                List<nodeinfo> list = new ArrayList<>();
+                pair ele = q.poll();
+                if (map.containsKey(ele.horizontalDistance)) {
+                    list = map.get(ele.horizontalDistance);
+                }
+                list.add(new nodeinfo(ele.node.val,lvl));
+                map.put(ele.horizontalDistance, list);
+                if (ele.node.left != null)
+                    q.offer(new pair(ele.horizontalDistance - 1, ele.node.left));
+                if (ele.node.right != null)
+                    q.offer(new pair(ele.horizontalDistance + 1, ele.node.right));
+            }
         }
 
-        // Sort keys (HDs)
-        List<Integer> keys = new ArrayList<>(mainMap.keySet());
-        Collections.sort(keys);
 
-        // Process each HD
-        for (Integer key : keys) {
-            List<nodeInfo> infoList = mainMap.get(key);
+        for(Map.Entry<Integer, List<nodeinfo>> entry :map.entrySet())
+        {
+            Collections.sort(entry.getValue(),
+            (a,b)->{
+                    if(a.lvl==b.lvl)
+                    return a.val-b.val; 
+                    else
+                    return a.lvl-b.lvl;
+                    }
+            );
 
-            // Sort by level first, then by value
-            Collections.sort(infoList, (a, b) -> {
-                if (a.level == b.level) return a.value - b.value;
-                return a.level - b.level;
-            });
+            List<Integer> temp = new ArrayList<>();
 
-            // Extract values only
-            List<Integer> col = new ArrayList<>();
-            for (nodeInfo info : infoList)
-                col.add(info.value);
+            for (nodeinfo ni : entry.getValue()) {
+                temp.add(ni.val);
+            }
+            ansList.add(temp);
 
-            ansList.add(col);
         }
-
+       
         return ansList;
     }
 }
