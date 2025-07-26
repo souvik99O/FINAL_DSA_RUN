ef374bf75a4ceed458321fad271557bf5e53ad8b
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-26 08:51:30 +0530

    Time: 571 ms (5.02%) | Memory: 45.6 MB (64.99%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0)
+            return false;
+
+        int TotalGroups = hand.length / groupSize;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : hand) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int j = 0; j < TotalGroups; j++) {
+            int minKey = Integer.MAX_VALUE;
+            for (int key : map.keySet()) {
+                if (key < minKey) {
+                    minKey = key;
+                }
+            }
+
+            map.put(minKey, map.getOrDefault(minKey, 0) - 1);
+            if(map.get(minKey)==0)
+            map.remove(minKey);
+
+            for(int i=0; i<groupSize-1;i++)
+            {
+                if(map.containsKey(minKey+1))
+                {
+                    minKey=minKey+1;
+                    map.put(minKey, map.getOrDefault(minKey, 0) - 1);
+                    if(map.get(minKey)==0)
+                    map.remove(minKey);
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(map.size()==0)
+        return true;
+
+        else
+        return false;
+    }
+}
