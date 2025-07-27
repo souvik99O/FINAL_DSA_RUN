7d349210581288864f7c9f5814d1cd2052eafea2
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-27 18:01:44 +0530

    Time: 1 ms (33.40%) | Memory: 41.9 MB (30.53%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,40 +1,28 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int count=0;
-        for(int i=1; i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i-1])
-            continue;
-            int l=i-1;
-            int r=i+1;
-            if(nums[r]==nums[i])
-            {
-                while(r<nums.length-1 && nums[r]==nums[i])
-                {
-                    r++;
-                }
+        int count = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i - 1])
+                continue;
+            int l = i - 1;
+            int r = i + 1;
 
-            }
-            if(nums[l]==nums[i])
-            {
-                while(l>0 && nums[l]==nums[i])
-                {
-                    l--;
-                }
+            while (r < nums.length - 1 && nums[r] == nums[i]) {
+                r++;
             }
 
-            
-            
-            if(nums[l]!=nums[i] && nums[r]!=nums[i])
-            {
-                if((nums[l]>nums[i] && nums[r]>nums[i])||(nums[l]<nums[i] && nums[r]<nums[i]))
-                {
+            while (l > 0 && nums[l] == nums[i]) {
+                l--;
+            }
+
+            if (nums[l] != nums[i] && nums[r] != nums[i]) {
+                if ((nums[l] > nums[i] && nums[r] > nums[i]) || (nums[l] < nums[i] && nums[r] < nums[i])) {
                     count++;
                 }
             }
         }
 
         return count;
-        
+
     }
 }
