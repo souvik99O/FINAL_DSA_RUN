9f1080428dae8c50f3539c22d408fa5832a29181
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-27 18:04:38 +0530

    Time: 1 ms (33.40%) | Memory: 41.6 MB (54.10%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -3,13 +3,14 @@ class Solution {
         int count = 0;
         for (int i = 1; i < nums.length - 1; i++) {
             if (nums[i] == nums[i - 1])
-                continue;
+            continue;
             int l = i - 1;
             int r = i + 1;
-
-            while (r < nums.length - 1 && nums[r] == nums[i]) {
+        
+            while (r < nums.length-1  && nums[r] == nums[i]) {
                 r++;
             }
+        
 
             while (l > 0 && nums[l] == nums[i]) {
                 l--;
