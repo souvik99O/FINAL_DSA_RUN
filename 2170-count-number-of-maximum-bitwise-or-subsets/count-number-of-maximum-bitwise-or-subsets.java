ba1453083334ed252e23547ccffa69018c707f91
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-07-28 18:18:32 +0530

    Time: 8 ms (80.38%) | Memory: 41.3 MB (55.22%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,37 +1,33 @@
 class Solution {
     HashMap<Integer,Integer> map= new  HashMap<>();
-    public int countMaxOrSubsets(int[] nums) {
-    
-    List<Integer> list= new ArrayList<>();
-    int maxXor=solve(0,list,nums,0);
-    
+    int count=0;
 
-    return map.get(maxXor);   
+    public int countMaxOrSubsets(int[] nums) {
+
+    int maxXor=0;
+
+    for(int i=0; i<nums.length;i++)
+    {
+       maxXor = maxXor | nums[i];
     }
 
-    public int solve(int i, List<Integer> list,int[]nums,int maxXor)
+    solve(0,0,nums,maxXor);
+    
+    //can i ooptimize this?
+    //offcourse yes'''''''
+    return count;   
+    }
+
+    public void solve(int i,int curOr,int[]nums,int maxXor)
     {
         int n=nums.length;
         if(i>=n)
         {
-            int xor=0;
-            for(int j=0; j<list.size();j++)
-            {
-              
-              xor= xor | list.get(j);   
-            }
-            
-            maxXor=Math.max(maxXor,xor);
-            map.put(xor,map.getOrDefault(xor,0)+1);
-            return maxXor;
-        }
-
-        list.add(nums[i]);
-        int pick=solve(i+1,list,nums,maxXor);
-        list.remove(list.size()-1);
-        int notPick=solve(i+1,list,nums,maxXor);
-
-        return Math.max(pick,notPick);
-
+            if(curOr==maxXor)
+            count++;
+            return;
+        } 
+        solve(i+1,curOr | nums[i],nums,maxXor);
+        solve(i+1,curOr,nums,maxXor);
     }
 }
