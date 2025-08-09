619cceb38204a3d9835a9ccf2ad3650ce36d566b
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-08-09 09:32:16 +0530

    Time: 1 ms (35.75%) | Memory: 41.3 MB (17.04%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,7 +3,7 @@ class Solution {
         int count=0;
         while(n>0)
         {
-            int bit=n%2;
+            int bit=n&1;
             n=n>>1;
             if(bit==1)
             count++;
