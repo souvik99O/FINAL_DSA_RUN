9ad648063e846abfa2404fbebccfbb2daa23dee0
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-08-16 21:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.99%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<1)return false;
+        while(n%4==0)
+        {
+            n=n/4;
+        }
+        if(n==1)
+       return true;
+       return false; 
+    }
+}
