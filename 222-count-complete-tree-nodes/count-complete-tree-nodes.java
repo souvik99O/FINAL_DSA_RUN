1fbdfc71c4952d7cf6d60ccad8818638c928af30
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-09-21 22:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (17.51%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -1,34 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     int count =0;
+//     public int countNodes(TreeNode root) {
+//         int ans=count(root);
+//         return ans;
+        
+//     }
+//     public int count(TreeNode root)
+//     {
+//         if(root==null)
+//         return 0;
+
+        
+//         int left=count(root.left);
+//         int right=count(root.right);
+
+//         return 1+left+right;
+//     }
+// }
+
+
+//O(log^2 n)
+
 class Solution {
+    int count =0;
     public int countNodes(TreeNode root) {
-        if (root == null) return 0;
-
-        int leftDepth = getLeftDepth(root);
-        int rightDepth = getRightDepth(root);
-
-        if (leftDepth == rightDepth) {
-            // It's a full binary tree
-            return (1 << leftDepth) - 1; // 2^height - 1
-        } else {
-            // Not full, recurse
-            return 1 + countNodes(root.left) + countNodes(root.right);
+        if(leftHieght(root)==rightHieght(root))
+        {
+            return (int)Math.pow(2,leftHieght(root))-1;
+        }
+        else
+        {
+            if(root==null)
+            return 0;
+            return 1+countNodes(root.left)+countNodes(root.right);
         }
     }
+    public int leftHieght(TreeNode root)
+    {
+        int depth=0;
 
-    private int getLeftDepth(TreeNode node) {
-        int depth = 0;
-        while (node != null) {
+        
+        while(root!=null)
+        {
             depth++;
-            node = node.left;
+            root=root.left;
         }
+
         return depth;
     }
+    public int rightHieght(TreeNode root)
+    {
+        int depth=0;
 
-    private int getRightDepth(TreeNode node) {
-        int depth = 0;
-        while (node != null) {
+        
+        while(root!=null)
+        {
             depth++;
-            node = node.right;
+            root=root.right;
         }
+
         return depth;
     }
 }
