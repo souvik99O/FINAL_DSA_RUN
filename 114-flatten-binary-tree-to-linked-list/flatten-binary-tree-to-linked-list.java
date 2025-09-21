f6989f30440d4f71b1c81566b9021d6e0f831ef9
Souvik Goswami <125156995+souvik99O@users.noreply.github.com>
2025-09-22 00:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.97%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -13,25 +13,43 @@
  *     }
  * }
  */
-class Solution {
-    List<TreeNode> preorderList =new ArrayList<>();
-    public void flatten(TreeNode root) {
-        preorder(root);
-        for(int i=0; i<preorderList.size()-1; i++)
-        {
-            preorderList.get(i).left=null;
-            preorderList.get(i).right=preorderList.get(i+1);
-        }
+// class Solution {
+//     List<TreeNode> preorderList =new ArrayList<>();
+//     public void flatten(TreeNode root) {
+//         preorder(root);
+//         for(int i=0; i<preorderList.size()-1; i++)
+//         {
+//             preorderList.get(i).left=null;
+//             preorderList.get(i).right=preorderList.get(i+1);
+//         }
         
-    }
-    public void preorder(TreeNode root)
-    {
+//     }
+//     public void preorder(TreeNode root)
+//     {
+//         if(root==null)
+//         return;
+        
+//         preorderList.add(root);
+//         preorder(root.left);
+//         preorder(root.right);
+
+//     }
+// }
+
+class Solution {
+    TreeNode prev=null;
+    public void flatten(TreeNode root) {
+        
         if(root==null)
         return;
-        
-        preorderList.add(root);
-        preorder(root.left);
-        preorder(root.right);
 
+        flatten(root.right);
+        flatten(root.left);
+
+        root.left=null;
+        root.right=prev;
+        prev=root;
+        
     }
+   
 }
